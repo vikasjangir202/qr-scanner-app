f667b81cab4eb4f6c199cc4f44f08ba4a46aee94
vikas <vikas@pop-os.localdomain>
2021-10-24 20:03:26 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <Text>Hello F</Text>
+    </SafeAreaView>
+  );
+}
+
+// const styles = StyleSheet.create({
+
+// });
+
+export default App;
