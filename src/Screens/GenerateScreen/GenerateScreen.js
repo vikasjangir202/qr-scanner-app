3642102d5d3f3511028d56a5552d6edcfb9b4cba
vikas <vikas@pop-os.localdomain>
2021-11-10 23:43:40 +0530

    boilerplate for generate screen

--- src/Screens/GenerateScreen/GenerateScreen.js
+++ src/Screens/GenerateScreen/GenerateScreen.js
@@ -1,12 +1,67 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import BottomNav from '../../components/BottomNav/BottomNav';
+import {colors} from '../../Configs/Colors';
 
 export default function GenerateScreen({navigation}) {
   return (
-    <View style={{display: 'flex', flex: 1}}>
-      <Text>GenerateScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text
+          style={[
+            styles.headerText,
+            {
+              fontSize: 20,
+            },
+          ]}>
+          Generate
+        </Text>
+      </View>
+
+      <View style={styles.content}></View>
+
       <BottomNav navigation={navigation} routeName="generate" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flex: 1,
+    backgroundColor: colors.lightWhite,
+  },
+  header: {
+    backgroundColor: colors.white,
+    width: '100%',
+    padding: 15,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  headerText: {
+    fontSize: 18,
+    color: colors.black,
+  },
+  content: {
+    backgroundColor: colors.white,
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  scannedGenerated: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: '100%',
+    padding: 3,
+  },
+  middleBorder: {width: 2, backgroundColor: colors.border},
+  Topbuttons: {
+    padding: 8,
+  },
+});
